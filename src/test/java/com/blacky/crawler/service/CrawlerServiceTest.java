b7c7858215d9937ecb52f1a285acdac717f1bbe4
Alexey Dushen <dushen.alexey@gmail.com>
2015-11-20 01:32:58 +0300

    Add some tests

--- src/test/java/com/blacky/crawler/service/CrawlerServiceTest.java
+++ src/test/java/com/blacky/crawler/service/CrawlerServiceTest.java
@@ -15,12 +15,28 @@ public class CrawlerServiceTest extends AbstractServiceTest {
     CrawlerService service;
 
 
-    // <editor-fold desc="0 test. CrawlerService">
+    // <editor-fold desc="1 test. CrawlerService.add(String domain, String keyword)">
     @Test
-    public void test() throws Exception {
+    public void add_one() throws Exception {
+        String domain = "example.com";
+        String keyword = "example";
+
+        service.add(domain, keyword);
+
         fail();
     }
     // </editor-fold>
 
+
+    // <editor-fold desc="1 test. CrawlerService.add(String domain, String keyword)">
+    @Test
+    public void get_result() throws Exception {
+        Integer id = 1;
+
+        service.get(id);
+
+        fail();
+    }
+    // </editor-fold>
 
 }
--- src/test/java/com/blacky/crawler/web/CrawlerRestTest.java
+++ src/test/java/com/blacky/crawler/web/CrawlerRestTest.java
@@ -3,6 +3,10 @@ package com.blacky.crawler.web;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * User: blacky
@@ -10,13 +14,38 @@ import static org.junit.Assert.*;
  */
 public class CrawlerRestTest extends AbstractRestTest {
 
+    public static final String REST_URL = CrawlerRest.REST_URL + '/';
+    public static final String REST_URL_NEW_ONE = REST_URL + "new";
 
-    // <editor-fold desc="0 test. CrawlerRest">
+
+    // <editor-fold desc="1 test. CrawlerRest.add()">
     @Test
-    public void test() throws Exception {
-        fail();
+    /**
+     * Positive test.
+     * Expect 200 OK.
+     */
+    public void add_expectOk() throws Exception {
+        String domain = "example.com";
+        String keyword = "example";
+
+        mockMvc.perform(post(REST_URL_NEW_ONE).param("domain", domain).param("keyword", keyword))
+                .andExpect(forwardedUrl(null))
+                .andExpect(status().isOk());
     }
     // </editor-fold>
 
+    // <editor-fold desc="1 test. CrawlerRest.get(int id)">
+    @Test
+    /**
+     * Positive test.
+     * Expect 200 OK.
+     */
+    public void get_expectOk() throws Exception {
+        Integer id = 1;
 
+        mockMvc.perform(get(REST_URL + id))
+                .andExpect(forwardedUrl(null))
+                .andExpect(status().isOk());
+    }
+    // </editor-fold>
 }
